2e34b7bb3ef0dd0095212dec00e810fd40f53ea6
ItzzS1d <sanamanisiddharam123@gmail.com>
2024-06-10 22:58:41 +0530

    error fix

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -8,7 +8,7 @@ export const protectedRoute = (req, res, next) => {
       return res.status(401).json({
         error: "No token, authorization denied",
       });
-    jwt.verify(token, "process.env.JWT_SECRET", async (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) return res.status(401).json({ error: "Token is not valid" });
       const user = await User.findById(decoded.id).select("-password");
       if (!user) return res.status(404).json({ error: "User not found" });
