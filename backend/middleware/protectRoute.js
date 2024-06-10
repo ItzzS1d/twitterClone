6f8da6d87acee1424bea59ba58c8cfa2367820f2
ItzzS1d <sanamanisiddharam123@gmail.com>
2024-06-10 23:22:20 +0530

    dfa

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -2,15 +2,21 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 export const protectedRoute = (req, res, next) => {
   const token = req.cookies.jwt;
-  
+  console.log(token)
+
   try {
-    if (!token)
+    if (!token) {
       return res.status(401).json({
         error: "No token, authorization denied",
       });
+    }
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-      if (err) return res.status(401).json({ error: "Token is not valid" });
+      if (err) {
+        console.log(err)
+        return res.status(401).json({ error: "Token is not valid" });
+      }
       const user = await User.findById(decoded.id).select("-password");
+      console.log(user)
       if (!user) return res.status(404).json({ error: "User not found" });
       req.user = user;
       next();
