32c93006160265249e9b88a906fdea1cdb334cfb
ItzzS1d <sanamanisiddharam123@gmail.com>
2024-06-10 22:28:22 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
+import cors from "cors";
+app.use(cors());
 import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
 const app = express();
-const port =  5000;
+const port =  process.env.PORT || 5000;
 const __dirname = path.resolve();
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
@@ -25,12 +26,12 @@ cloudinary.config({
   secure: true,
 });
 
-
-  // app.use(express.static(path.join(__dirname, "frontend/dist")));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  // });
-
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
